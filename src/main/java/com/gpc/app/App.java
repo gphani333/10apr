f971ce154874b97a400ddef42f277fd66ba79cef
gphani333 <81351497+gphani333@users.noreply.github.com>
2021-04-10 13:36:48 +0530

    files add

---
+++ src/main/java/com/gpc/app/App.java
@@ -0,0 +1,13 @@
+package com.gpc.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Phani!" );
+    }
+}
